cd621b09f3bae80e86b69a3ad48f9790131c69d6
Abishek0612 <uabishek6@gmail.com>
2025-03-31 22:00:20 +0530

    Tailwind css setup and assets and metadata setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-const Home = () => <h1 className="text-5xl text-white">Welcome to home</h1>;
+const Home = () => (
+  <>
+    <h1 className="h1-bold font-inter">Welcome to home</h1>
+    <h1 className="h1-bold  font-space-grotesk ">Welcome to home</h1>
+  </>
+);
 
 export default Home;
+
+//.text-dark100_light900 or .background-light850_dark100
