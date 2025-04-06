90583b7821efae6738c57b098fe1d664168e3762
Abishek0612 <uabishek6@gmail.com>
2025-04-06 13:11:39 +0530

    Set up Light and dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 const Home = () => (
   <>
-    <h1 className="h1-bold font-inter">Welcome to home</h1>
-    <h1 className="h1-bold  font-space-grotesk ">Welcome to home</h1>
+    <h1 className="h1-bold bg-black ">Welcome to home</h1>
+    <h1 className="h1-bold  font-inter ">Welcome to home</h1>
+    <h1 className="h1-bold  font-light ">Welcome to home</h1>
+    <h1 className="h1-bold  font-semibold ">Welcome to home</h1>
   </>
 );
 
 export default Home;
 
-//.text-dark100_light900 or .background-light850_dark100
+// .text-dark100_light900 or .background-light850_dark100
